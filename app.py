7c254f9b259712c78213ef5a3d115bb854d3c4ec
Aman Sharma <amanramasharma@gmail.com>
2025-05-10 03:04:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ client = bigquery.Client()
 
 @app.route("/")
 def index():
-    query = "SELECT category, COUNT(*) AS order_count FROM `comm034coursework-6875078.thelook.order_items` GROUP BY category ORDER BY order_count DESC LIMIT 5"
-    result = client.query(query).to_dataframe()
-    return result.to_html()
+    query = """
+        SELECT p.category, COUNT(*) AS order_count
+        FROM `comm034coursework-6875078.thelook.order_items` AS oi
+        JOIN `comm034coursework-6875078.thelook.products` AS p
+        ON oi.product_id = p.id
+        GROUP BY p.category
+        ORDER BY order_count DESC
+        LIMIT 5
+    """
+    df = client.query(query).to_dataframe()
+    return df.to_html()
